2785d6f31744999e956d6c62559621980e10025a
Ramón París <rparis@retailsbs.com>
2021-11-22 01:31:28 -0300

    creating account for total gifs

--- solana-gif-portal-program/programs/solana-gif-portal-program/src/lib.rs
+++ solana-gif-portal-program/programs/solana-gif-portal-program/src/lib.rs
@@ -5,10 +5,43 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod solana_gif_portal_program {
     use super::*;
+
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    // Another function woo!
+    pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult {
+        // Get a reference to the account and increment total_gifs.
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
+
+// Specify what data you want in the AddGif Context.
+// Getting a handle on the flow of things :)?
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
--- solana-gif-portal-program/tests/solana-gif-portal-program.ts
+++ solana-gif-portal-program/tests/solana-gif-portal-program.ts
@@ -2,16 +2,43 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolanaGifPortalProgram } from "../target/types/solana_gif_portal_program";
 
+const { SystemProgram } = anchor.web3;
+
 const main = async () => {
   console.log("🚀 Starting test...");
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  let provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace
     .SolanaGifPortalProgram as Program<SolanaGifPortalProgram>;
-  let tx = await program.rpc.initialize({});
+
+  // Creates an account key pair for our program to use
+  const baseAccount = anchor.web3.Keypair.generate();
+  let tx = await program.rpc.initialize({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      systemProgram: SystemProgram.programId,
+      user: provider.wallet.publicKey,
+    },
+    signers: [baseAccount],
+  });
   console.log("📝 Your transaction signature", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // Call add_gif!
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+
+  // Get the account again to see what changed.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
 };
 
 const runMain = async () => {
