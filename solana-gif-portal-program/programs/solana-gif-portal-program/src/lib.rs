651dd218feaaf6efd77303d3cf30020c9ddcd9c2
Ramón París <rparis@retailsbs.com>
2021-11-22 19:39:47 -0300

    deploying in dev

--- solana-gif-portal-program/programs/solana-gif-portal-program/src/lib.rs
+++ solana-gif-portal-program/programs/solana-gif-portal-program/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("9SF35gQGQH1r3vcAvTo8BhYs99fYXwDmKRJDArezcVYQ");
 
 #[program]
 pub mod solana_gif_portal_program {
